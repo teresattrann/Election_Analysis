27a4fc7f79b1a4959c2e8f725a527781b517c3ae
teresattrann <98780937+teresattrann@users.noreply.github.com>
2022-03-08 12:32:14 -0800

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -2,9 +2,9 @@
 import csv
 import os
 # Assign a variable to load a file from a path.
-file_to_load = os.path.join("election_results.csv")
+file_to_load = os.path.join("Resources/election_results.csv")
 # Assign a variable to save the file to a path.
-file_to_save = os.path.join("election_analysis.txt")
+file_to_save = os.path.join("Resources/election_results.txt")
 # Initialize a total vote counter.
 total_votes = 0
 # Candidate options and candidate votes.
